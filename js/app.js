e8234dbb305378c998fea345b82f0a152dbc63f2
Simon Ball <simonball245@gmail.com>
2024-05-24 14:33:46 +0200

    Add methods to display calorie data

--- js/app.js
+++ js/app.js
@@ -4,16 +4,67 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+
+    this._displayCaloriesLimit();
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
   }
 
+  // Public Methods
   addMeal(meal) {
     this._meals.push(meal);
     this._totalCalories += meal.calories;
+    this._render();
   }
 
   addWorkout(workout) {
-    this._meals.push(workout);
+    this._workouts.push(workout);
     this._totalCalories -= workout.calories;
+    this._render();
+  }
+
+  //   Private Methods
+  _displayCaloriesTotal() {
+    const totalCaloriesEl = document.getElementById('calories-total');
+    totalCaloriesEl.innerHTML = this._totalCalories;
+  }
+
+  _displayCaloriesLimit() {
+    const calorieLimitEl = document.getElementById('calories-limit');
+    calorieLimitEl.innerHTML = this._calorieLimit;
+  }
+
+  _displayCaloriesConsumed() {
+    const caloriesConsumedEl = document.getElementById('calories-consumed');
+
+    caloriesConsumedEl.innerHTML = this._meals.reduce((total, meal) => {
+      console.log(total);
+      return total + meal.calories;
+    }, 0);
+  }
+
+  _displayCaloriesBurned() {
+    const caloriesBurnedEl = document.getElementById('calories-burned');
+
+    caloriesBurnedEl.innerHTML = this._workouts.reduce(
+      (total, meal) => total + meal.calories,
+      0,
+    );
+  }
+
+  _displayCaloriesRemaining() {
+    const caloriesRemainingEl = document.getElementById('calories-remaining');
+
+    caloriesRemainingEl.innerHTML = this._calorieLimit - this._totalCalories;
+  }
+
+  _render() {
+    this._displayCaloriesTotal();
+    this._displayCaloriesConsumed();
+    this._displayCaloriesBurned();
+    this._displayCaloriesRemaining();
   }
 }
 
