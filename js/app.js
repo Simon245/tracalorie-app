fd7227cf2d1b530d9c0d227b6ebc20774f18378a
Simon Ball <simonball245@gmail.com>
2024-05-24 16:46:19 +0200

    Add set limit modal functionality

--- js/app.js
+++ js/app.js
@@ -55,6 +55,12 @@ class CalorieTracker {
     this._render();
   }
 
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit;
+    this._displayCaloriesLimit();
+    this._render();
+  }
+
   //   Private Methods
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById('calories-total');
@@ -213,6 +219,9 @@ class App {
     document
       .getElementById('reset')
       .addEventListener('click', this._reset.bind(this));
+    document
+      .getElementById('limit-form')
+      .addEventListener('submit', this._setLimit.bind(this));
   }
 
   _newItem(type, e) {
@@ -273,6 +282,22 @@ class App {
     document.getElementById('workout-items').innerHTML = '';
     document.getElementById('filter-meals').value = '';
     document.getElementById('filter-workouts').value = '';
+  }
+
+  _setLimit(e) {
+    e.preventDefault();
+    const limit = document.getElementById('limit');
+    if (limit.value === '') {
+      alert('Please add a limit');
+      return;
+    }
+
+    this._tracker.setLimit(+limit.value);
+    limit.value = '';
+
+    const modalElement = document.getElementById(`limit-modal`);
+    const modal = bootstrap.Modal.getInstance(modalElement);
+    modal.hide();
   }
 }
 
