61d979c648d2ceeaa8b0859c7c1f3e39812f6020
Simon Ball <simonball245@gmail.com>
2024-05-24 17:18:46 +0200

    Move event listeners to own function

--- js/app.js
+++ js/app.js
@@ -272,6 +272,11 @@ class Storage {
 class App {
   constructor() {
     this._tracker = new CalorieTracker();
+    this._loadEventListeners();
+    this._tracker.loadItems();
+  }
+
+  _loadEventListeners() {
     document
       .getElementById('meal-form')
       .addEventListener('submit', this._newItem.bind(this, 'meal'));
@@ -296,7 +301,6 @@ class App {
     document
       .getElementById('limit-form')
       .addEventListener('submit', this._setLimit.bind(this));
-    this._tracker.loadItems();
   }
 
   _newItem(type, e) {
