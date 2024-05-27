d8622a2c70d5a6f7af9499d1dbf177f1a283c6c9
Simon Ball <simonball245@gmail.com>
2024-05-27 11:00:06 +0200

    Add function to reset day

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ class CalorieTracker {
     this._displayCaloriesBurned();
     this._displayCaloriesRemaining();
     this._displayCaloriesProgress();
+
+    document.getElementById('limit').value = this._calorieLimit;
   }
 
   // Public Methods
@@ -60,6 +62,7 @@ class CalorieTracker {
     this._totalCalories = 0;
     this._meals = [];
     this._workouts = [];
+    Storage.clearAll();
     this._render();
   }
 
@@ -290,6 +293,12 @@ class Storage {
     });
 
     localStorage.setItem('workouts', JSON.stringify(workouts));
+  }
+
+  static clearAll() {
+    localStorage.removeItem('workouts');
+    localStorage.removeItem('meals');
+    localStorage.removeItem('totalCalories');
   }
 }
 
